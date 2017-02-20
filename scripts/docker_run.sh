106a2cd14706ea28719bfe853d60094bf562da02
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2017-02-20 17:06:35 +0300

    try build docker

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-echo "HELLO!!!"
-
 #
 # Intended for CI systems to use when running test suite or deploying.
 #
@@ -14,5 +11,6 @@ set -e
 
 
 # Build image and run
-docker build -t jacekmarchwicki/android:java7-8 .
+#docker build -t jacekmarchwicki/android:java7-8 .
+docker run --tty --interactive --rm 1b372b1f76f2 /bin/bash
 docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm jacekmarchwicki/android:java7-8 /bin/sh -c "./gradlew build"

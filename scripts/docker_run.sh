430004d1158d74cb8e7600f50ef92055b02d911a
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2017-02-20 17:09:03 +0300

    add pull cmd

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -11,6 +11,7 @@ set -e
 
 
 # Build image and run
-#docker build -t jacekmarchwicki/android:java7-8 .
-docker run --tty --interactive --rm 1b372b1f76f2 /bin/bash
+
+docker pull stackd/sabre-dav
+docker build -t jacekmarchwicki/android:java7-8 .
 docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm jacekmarchwicki/android:java7-8 /bin/sh -c "./gradlew build"

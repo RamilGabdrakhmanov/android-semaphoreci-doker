3f2d6f54ea07f16c9c3de6278246fe37f472f7e6
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2017-03-03 15:47:38 +0300

    add more steps for ci

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,14 @@
+echo "!!!!CI BEGIN 1!!!!!"
+
 #!/bin/bash
 
 # Fail immediately
+echo "!!!!CI BEGIN 2!!!!!"
 set -e
 
+echo "!!!!CI BEGIN 3!!!!!"
 cd app
+
 
 echo "!!!!CI BEGIN!!!!!"
 echo "!!!!CI BEGIN!!!!!"

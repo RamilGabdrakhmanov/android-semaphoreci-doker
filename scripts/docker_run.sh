2f298978b165239dc59b76ad999d7200a0f0fe1c
Ramil Gabdrakhmanov <ramil.gabdrakhmanov@flatstack.com>
2017-03-13 18:35:06 +0300

    remove initial build

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -12,7 +12,11 @@ set -e
 
 # Build image and run
 docker build -t gfx/docker-android-project .
-docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm gfx/docker-android-project /bin/sh -c "./gradlew build"
+docker run --tty --interactive --volume=$(pwd):/opt/workspace --workdir=/opt/workspace --rm gfx/docker-android-project
+ls
+rm local.properties
+ls
+./gradlew tasks
 
 echo "!!!!RUN COMPLETE!!!!!!"
 echo "!!!!RUN COMPLETE!!!!!!"
